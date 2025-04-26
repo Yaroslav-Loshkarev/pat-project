e66bae35ce7574071c7e271fcefa926acbee5cb1
Yaroslav <kseltas28@gmail.com>
2025-04-26 22:28:11 +0300

    Deletd unneeded code strings

--- index.php
+++ index.php
@@ -3,19 +3,7 @@
 	<body>
 		<?
 		require_once 'Client.php';
-		$client = new Client("Yaroslav", "n128a7@mail.ru");
-		
-		//echo $client;
-		echo "<br>";
-		echo Client::showCountOfclassObjects();
-		echo "<br>";
-		print_r(get_class_methods('client'));
-		echo "<br>";
-		if(method_exists($client, 'increaseNumberOfObjects')){
-			echo "dddd";
-		}
-		echo "<br>";
-		echo (property_exists('client', 'name'));
+		$client = new Client("Yaroslav", "n128a7@mail.ru");	
 		?>		
 	</body>
 </html>
